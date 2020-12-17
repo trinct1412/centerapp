c5cf74d9370b1b7cdc87acd02f171b52a067987a
trinct1412 <trinct1412@gmail.com>
2020-12-17 21:11:41 +0700

    deloy center app network heroku

--- center/settings.py
+++ center/settings.py
@@ -166,9 +166,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 DEBUG_DIRECTED_FILE = "django-logging"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+
 # Config celery
 BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
