ec84e8d3f6107e55468cca784d2b879b8241a1f0
trinct1412 <trinct1412@gmail.com>
2020-12-17 21:10:23 +0700

    deloy center app network heroku

--- center/settings.py
+++ center/settings.py
@@ -161,12 +161,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 DEBUG_DIRECTED_FILE = "django-logging"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 # Config celery
 BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
