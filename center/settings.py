add997a5649909cf7d2aad3949d67c131d4063e8
trinct1412 <trinct1412@gmail.com>
2020-12-17 20:45:58 +0700

    deloy center app network heroku

--- center/settings.py
+++ center/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+#create location save logging file
+if not os.path.exists('django-logging'):
+    os.mkdir('django-logging')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
